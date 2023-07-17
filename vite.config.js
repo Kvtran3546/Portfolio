e149750a3162cd102df5723abf78e7d990d2f881
Khoa <Kvtran3546@gmail.com>
2023-07-17 12:45:57 -0400

    fdas

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'development'? '/Portfolio' : './',
+
   plugins: [react()],
+  base: "/Portfolio/"
 })
